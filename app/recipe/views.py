cbea250820ee1bd09f06089281b0cef9a15164ee
gertzakis <gerasimos.tzakis@networktocode.com>
2022-11-03 14:14:11 +0200

    add tags API endpoints

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 """Views for the Recipe APIs."""
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -20,7 +20,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
-
         if self.action == "list":
             return serializers.RecipeSerializer
 
@@ -29,3 +28,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """View for manage tags."""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
